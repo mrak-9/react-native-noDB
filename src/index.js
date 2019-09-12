1dbae1300d9d09aa55df7b9e51aa02a16f340a5e
mrak <dmatushin@directmedia.ru>
2019-09-12 14:48:21 +0300

    process

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Datastore = require('nedb');
-
-module.exports = function(options = {}) {
-	if (!options.storage && !options.inMemoryOnly) {
-		options.storage = require('./storage');
-	}
-	
-	return new Datastore(options);
-}
